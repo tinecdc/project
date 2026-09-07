1cd293ede41e432be2929f78ad6aed8fd04b33f1
tinecdc <133973246+tinecdc@users.noreply.github.com>
2026-09-07 09:41:05 +0800

    Update Program.tsx

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -6,7 +6,7 @@ const programs = [
     title: '18 Roses',
     people: [
       'Engr. Fernando Miranda',  'Mr. Roldan Lucena', 'Mr. Frederic Natividad', 'Mr. Reynante Cu','Mr. Benito "Benjie" Miranda','Mr. Pepito Marquez', 'Mr. Conrado Miranda', 'Mr. John Persian Cu', 'Mr. Paolo Garcia', 'Mr. Jam Dela Fuente', 'Mr. Lawrence Cu'
-      , 'Mr. Jerriel Ochoco', 'Mr. Seung Hyun Kim', 'Mr. Giro Hinolan', 'Mr. Domingo Hernandez Jr.','Mr. Jared Reyes','Mr. Floyd Urrutia',  'Mr. Jorge Miranda',
+      , 'Mr. Carlos Labayno', 'Mr. Seung Hyun Kim', 'Mr. Giro Hinolan', 'Mr. Domingo Hernandez Jr.', 'Mr. Gaib Galvez', 'Mr. Floyd Urrutia',  'Mr. Jorge Miranda',
     ],
   },
   {
@@ -19,7 +19,7 @@ const programs = [
   {
     title: '18 Gifts',
     people: [
-      'Mr. Elmo Lumba','Ms. Audrey Rosales', 'Ms. Angelique Lalicon', 'Ms. Julianna De Guzman', 'Ms. Aaliyah Go', 'Ms. Maan Del Rosario', 'Mr. Dan Allen Pepanio', 'Ms. Teffany Montaraye', 'Mr. Felix Enriquez', 'Ms. Bettina Yan',
+      'Mr. Elmo Lumba','Ms. Audrey Rosales', 'Ms. Angelique Lalicon', 'Ms. Julianna De Guzman', 'Ms. Aaliyah Go', 'Ms. Maan Del Rosario', 'Mr. Dan Allen Pepanio', 'Ms. Khendy May Baviera', 'Mr. Felix Enriquez', 'Ms. Bettina Yan',
       'Mr. Josef Buyco', 'Ms. Margarette Reyes', 'Mr. Lebron Mapue', 'Ms. Mischa Castro', 'Ms. Julliana Garcia', 'Ms. Anika Boydon', 'Ms. Janelle Joaquin', 'Ms. Jana Boydon', 
     ],
   },
@@ -33,8 +33,8 @@ const programs = [
   {
     title: '18 Shots',
     people: [
-      'Mr. Carlos Labayno', 'Ms. Moira Okutumo',  'Mr. Camherzon Osiones', 'Mr. Cian Guison', 'Mr. Kevin Dela Vega', 'Ms. Louie Lopez',  'Mr. Siegfried Mariano', 'Mr. Youli Santiago', 'Ms. Iya Mendoza',
-       'Mr. Migz Valimento', 'Ms. Heather Trinidad', 'Mr. John Paul Hermogenes', 'Ms. Dhyana Estrella', 'Ms. Princess Jaenne Cruz',  'Ms. Camilla Celestino', 'Ms. Mariel Mauricio', 'Ms. Pauleen Inocentes', 'Mr. Gaib Galvez'
+      'Mr. Rafael Rei Panganiban', 'Ms. Moira Okutumo',  'Mr. Camherzon Osiones', 'Mr. Cian Guison', 'Mr. Kevin Dela Vega', 'Ms. Louie Lopez',  'Mr. Siegfried Mariano', 'Mr. Youli Santiago', 'Ms. Iya Mendoza',
+       'Mr. Migz Valimento', 'Ms. Heather Trinidad', 'Mr. John Paul Hermogenes', 'Ms. Dhyana Estrella', 'Ms. Princess Jaenne Cruz', 'Ms. Mariel Mauricio', 'Ms. Pauleen Inocentes', 'Mr. Jared Reyes',
     ],
   },
 
